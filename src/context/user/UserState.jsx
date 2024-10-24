95465e839dab1fb802406d48bff0775862001fca
BALANSH24 <148430085+BALANSH24@users.noreply.github.com>
2024-10-24 15:36:25 +0530

    Update UserState.jsx

--- src/context/user/UserState.jsx
+++ src/context/user/UserState.jsx
@@ -14,7 +14,7 @@ const isPasswordValid = (password) => {
 
 const UserProvider = ({ children }) => {
   // URL:
-  let base = "http://localhost:5500";
+  let base = "https://todobackend-fl1b.onrender.com";
   let path = "/user/";
   let url = new URL(path, base).href;
 
